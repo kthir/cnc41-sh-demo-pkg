8404a4c86a7f93d9089f2a34bbf1c862c2a0c924
Krishnan Thirukonda <tbk@cisco.com>
2022-11-30 09:31:59 -0500

    working version of plugin w advanced

--- custom/Plugin/demo_l3vpn_plugin.py
+++ custom/Plugin/demo_l3vpn_plugin.py
@@ -18,11 +18,10 @@ import ipaddress
 
 # dep_namespace:
 def run(xml_object, dep_class, dep_name, dep_namespace, test_run, rule_name, rule_namespace):
-    result = {}
     result_list = []
     utf8_parser = ET.XMLParser(encoding='utf-8', recover=True)
     root = ET.fromstring(xml_object.encode('utf-8'), parser=utf8_parser)
-    #print(ET.tostring(root, pretty_print=True))
+
     # #### To remove the namespace if its present
     for elem in root.iter():
         if not hasattr(elem.tag, 'find'): continue  # (1)
@@ -30,8 +29,6 @@ def run(xml_object, dep_class, dep_name, dep_namespace, test_run, rule_name, rul
         if i >= 0:
             elem.tag = elem.tag[i + 1:]
     objectify.deannotate(root, cleanup_namespaces=True)
-    #print(ET.tostring(root, pretty_print=True))
-    # ####
 
     try:
         if dep_class == "subservice.interface.health" and dep_name == "vpn-if-health-list" and \
@@ -41,11 +38,9 @@ def run(xml_object, dep_class, dep_name, dep_namespace, test_run, rule_name, rul
         elif rule_name == "Rule-L3VPN-Demo" and rule_namespace == "custom" and \
                 dep_class == "subservice.ebgp.nbr.health":
             return extract_subservice_ebgp_nbr_health_payload(root, test_run)
-
         elif rule_name == "Rule-L3VPN-Demo" and rule_namespace == "custom" and \
                 dep_class == "subservice.vrf.plain.lsp.reachability":
             return extract_subservice_vrf_plain_lsp_reachability_payload(root, test_run)
-
         else:
             return result_list
     except Exception as exception:
@@ -59,29 +54,26 @@ def extract_subservice_vrf_plain_lsp_reachability_payload(root, test_run):
         result_list = ["device", "vrf", "peer-vpn-addr-list"]
         return result_list
 
-    #device_records = root.findall('.//devices/device/key')
-    #devices = [device_record.text for device_record in device_records]
-
-    devices=get_pe_devices(root)
+    devices = get_pe_devices(root)
 
     for device in devices:
         # Each result is a dictionary that can be used to instantiate one Subservice instance
         result = {}
         result["device"] = device
 
-        device_vrf_path = ".//devices/device[name='{device}']/config/vrf/vrf-list/name".format(device=device)
+        device_vrf_path = ".//devices/device[key='{device}']/config/vrf/vrf-list/key".format(device=device)
         device_vrf_records = root.findall(device_vrf_path)
         result["vrf"] = device_vrf_records[0].text
 
         # IP address on this PE side
 
-        self_vpn_ip_path = ".//devices/device[name='{device}']/config/router/bgp/*/*/neighbor/id".format(device=device)
+        self_vpn_ip_path = ".//devices/device[key='{device}']/config/router/bgp/*/*/neighbor/key".format(device=device)
         self_vpn_ip_records = root.findall(self_vpn_ip_path)
         self_vpn_ip = self_vpn_ip_records[0].text
 
         # PE configs have vrf in it, but CE dont, so the following should work.
 
-        all_vpn_ip_records_path = ".//devices/device/config/router/bgp/*/vrf/neighbor/id"
+        all_vpn_ip_records_path = ".//devices/device/config/router/bgp/*/vrf/neighbor/key"
         all_vpn_ip_records = root.findall(all_vpn_ip_records_path)
 
         # Fetch all IP addresses except for self one
@@ -96,7 +88,7 @@ def extract_subservice_vrf_plain_lsp_reachability_payload(root, test_run):
                 peer_ntw_str_32 = "%s" % (ip_intf.network)
                 # hardcode mask to /24
                 peer_ntw_str_24 =str(ipaddress.ip_network(peer_ntw_str_32).supernet(new_prefix=24))
-                
+
                 peer_ntw_list.append(peer_ntw_str_24)
 
         result["peer-vpn-addr-list"] = peer_ntw_list
@@ -105,6 +97,29 @@ def extract_subservice_vrf_plain_lsp_reachability_payload(root, test_run):
 
     return result_list
 
+
+def extract_subservice_vpn_interfaces_payload(root, test_run):
+    result_list = []
+
+    if test_run:
+        result_list = ["device", "ifId"]
+        return result_list
+
+    for device in get_pe_devices(root):
+        # Each result is a dictionary that can be used to instantiate one Subservice instance
+        result = {}
+        result["device"] = device
+
+        # Assumes exactly one PE-CE interface on this vrf/vpn
+        interfacename_path=".//devices/device[key='{device}']/config/interface/*".format(device=device)
+        interfaceid_path=".//devices/device[key='{device}']/config/interface/*/key".format(device=device)
+
+        device_interfacename_record = root.xpath(interfacename_path)
+        device_interfaceid_record = root.xpath(interfaceid_path)
+        result["ifId"] = device_interfacename_record[0].tag + device_interfaceid_record[0].text
+        result_xml = json2xml.Json2xml(result, wrapper="plugin-output", pretty=True, attr_type=False).to_xml()
+        result_list.append(result_xml)
+    return result_list
 
 # Payload Extraction logic for subservice.ebgp.nbr.health
 def extract_subservice_ebgp_nbr_health_payload(root, test_run):
@@ -120,25 +135,26 @@ def extract_subservice_ebgp_nbr_health_payload(root, test_run):
         # Each result is a dictionary that can be used to instantiate one Subservice instance
         result = {}
         result["device"] = device
-    
-        device_vrf_path = ".//devices/device[name='{device}']/config/vrf/vrf-list/name".format(device=device)
+
+        device_vrf_path = ".//devices/device[key='{device}']/config/vrf/vrf-list/key".format(device=device)
         device_vrf_records = root.findall(device_vrf_path)
         result["vrf"] = device_vrf_records[0].text
 
         # Assumes exactly one eBGP peering
 
-        ebgp_address_family_path = ".//devices/device[name='{device}']/config/router/bgp/*/*/neighbor/address-family/*".format(device=device)
+        ebgp_address_family_path = ".//devices/device[key='{device}']/config/router/bgp/*/*/neighbor/address-family/*".format(device=device)
         ebgp_address_family_records = root.findall(ebgp_address_family_path)
 
         #ebgp_config_path = ".//flat-L3vpn/endpoint[access-pe='{device}']/ce-pe-prot/e-bgp".format(device=device)
         #ebgp_config_records = root.findall(ebgp_config_path)
         #if len(ebgp_config_records) == 0:
-            #continue
+        #continue
 
         # Not sure this is used currently, hardcode to address family
         result["bgp_nbr_type"] = ebgp_address_family_records[0].tag
 
-        bgp_nbr_ipaddrs_path = ".//devices/device[name='{device}']/config/router/bgp/*/*/neighbor/id".format(device=device)
+        bgp_nbr_ipaddrs_path = ".//devices/device[key='{device}']/config/router/bgp/*/*/neighbor/key".format(
+            device=device)
         bgp_nbr_ipaddrs_records = root.findall(bgp_nbr_ipaddrs_path)
         if len(bgp_nbr_ipaddrs_records) == 0:
             continue
@@ -153,65 +169,11 @@ def extract_subservice_ebgp_nbr_health_payload(root, test_run):
     return result_list
 
 
-def extract_subservice_vpn_interfaces_payload(root, test_run):
-    #print("Entering extract_subservice_vpn_interfaces_payload")
-    result_list = []
 
-    if test_run:
-        result_list = ["device", "ifId"]
-        return result_list
-
-
-    #device_records = root.findall('.//devices/device/name')
-    #devices = [device_record.text for device_record in device_records]
-
-    #pe_devices=get_pe_devices(root)
-    #print("walking all devices")
-
-    for device in get_pe_devices(root):
-        # Each result is a dictionary that can be used to instantiate one Subservice instance
-        result = {}
-        result["device"] = device
-        #print("device="+device)
-        #if device not in pe_devices : continue
-        #print("device is a PE")
-        # Assumes exactly one PE-CE interface on this vrf/vpn
-        interfacename_path=".//devices/device[name='{device}']/config/interface/*".format(device=device)
-        interfaceid_path=".//devices/device[name='{device}']/config/interface/*/id".format(device=device)
-
-        #interface_path = ".//vpn-nodes/vpn-node[ne-id='{device}']/vpn-network-accesses/vpn-network-access/port-id".format(
-            #device=device)
-        device_interfacename_record = root.xpath(interfacename_path)
-        #print("Got " + str(len(device_interfacename_record)) + " Records")
-        #for device_interfacename in device_interfacename_record:
-            #print(device_interfacename.tag)     
-            #print(device_interfacename)     
-
-        device_interfaceid_record = root.xpath(interfaceid_path)
-        #vlan_interface_path = ".//vpn-nodes/vpn-node[ne-id='{device}']/vpn-network-accesses/vpn-network-access/connection/tagged-interface/dot1q-vlan-tagged/cvlan-id".format(
-            #device=device)
-        #vlan_interface_record = root.xpath(vlan_interface_path)
-
-        #if len(vlan_interface_record) != 0:
-            #result["ifId"] = device_interface_record[0].text + "." + vlan_interface_record[0].text
-        #else:
-            #result["ifId"] = device_interface_record[0].text
-       
-
-        result["ifId"] = device_interfacename_record[0].tag + device_interfaceid_record[0].text
-
-        result_xml = json2xml.Json2xml(result, wrapper="plugin-output", pretty=True, attr_type=False).to_xml()
-        result_list.append(result_xml)
-
-    return result_list
-
-
-def get_pe_devices (root):
-    #print("Getting list of PEs")
-    xpathquery=".//pe-device"
-    pe_device_records=root.xpath(xpathquery)
+def get_pe_devices(root):
+    xpathquery = ".//endpoint/pe-device"
+    pe_device_records = root.findall(xpathquery)
     pe_devices = [pe_device_record.text for pe_device_record in pe_device_records]
-    #print(pe_devices)
     return pe_devices
 
 
